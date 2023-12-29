3ba300b0ab9b1b09956c15e5c2b4c62f11a761ad
Lai Jiangshan <jiangshan.ljs@antgroup.com>
2023-12-29 14:27:19 +0800

    KVM: x86/PVM: Implement module initialization related callbacks
    Implement hardware enable/disable and setup/unsetup callbacks for PVM
    module initialization.
    
    Signed-off-by: Lai Jiangshan <jiangshan.ljs@antgroup.com>
    Signed-off-by: Hou Wenlong <houwenlong.hwl@antgroup.com>

--- arch/x86/kvm/pvm/pvm.h
+++ arch/x86/kvm/pvm/pvm.h
@@ -2,6 +2,8 @@
 #ifndef __KVM_X86_PVM_H
 #define __KVM_X86_PVM_H
 
+#include <linux/kvm_host.h>
+
 #define PT_L4_SHIFT		39
 #define PT_L4_SIZE		(1UL << PT_L4_SHIFT)
 #define DEFAULT_RANGE_L4_SIZE	(32 * PT_L4_SIZE)
@@ -19,5 +21,23 @@ extern u64 *host_mmu_root_pgd;
 
 void host_mmu_destroy(void);
 int host_mmu_init(void);
+
+struct vcpu_pvm {
+	struct kvm_vcpu vcpu;
+};
+
+struct kvm_pvm {
+	struct kvm kvm;
+};
+
+static __always_inline struct kvm_pvm *to_kvm_pvm(struct kvm *kvm)
+{
+	return container_of(kvm, struct kvm_pvm, kvm);
+}
+
+static __always_inline struct vcpu_pvm *to_pvm(struct kvm_vcpu *vcpu)
+{
+	return container_of(vcpu, struct vcpu_pvm, vcpu);
+}
 
 #endif /* __KVM_X86_PVM_H */
