8d862bfbbfb17af6a43ff251b00d62b11c52b5ff
Hou Wenlong <houwenlong.hwl@antgroup.com>
2024-01-10 19:18:23 +0800

    KVM: x86: Implement gpc refresh for guest usage
    PVM uses pfncache to share the PVCS structure between the guest and
    host. The flag of pfncache of PVCS is initialized as
    KVM_GUEST_AND_HOST_USE_PFN because the PVCS is used inside the
    vcpu_run() callback, even in the switcher, where the vcpu is in guest
    mode. However, there is no real usage for GUEST_USE_PFN, so the request
    in mmu_notifier only kicks the vcpu out of guest mode and no refresh is
    done before the next vcpu_run(). Therefore, a new request type is
    introduced to request the refresh, and a new callback is used to service
    the request.
    
    Suggested-by: Lai Jiangshan <jiangshan.ljs@antgroup.com>
    Signed-off-by: Hou Wenlong <houwenlong.hwl@antgroup.com>

--- arch/x86/kvm/mmu/mmu.c
+++ arch/x86/kvm/mmu/mmu.c
@@ -7212,6 +7212,7 @@ void kvm_arch_flush_shadow_memslot(struct kvm *kvm,
 		kvm_mmu_zap_all_fast(kvm);
 	else
 		kvm_mmu_zap_memslot(kvm, slot);
+	kvm_make_all_cpus_request(kvm, KVM_REQ_GPC_REFRESH);
 }
 
 void kvm_mmu_invalidate_mmio_sptes(struct kvm *kvm, u64 gen)
