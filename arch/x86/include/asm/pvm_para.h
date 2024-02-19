6720badb2c62bd32f607bae46fe6c8b53c7d59a4
Lai Jiangshan <jiangshan.ljs@antgroup.com>
2024-02-19 15:04:03 +0800

    x86/pvm: Detect PVM hypervisor support
    Detect PVM hypervisor support through the use of the PVM synthetic
    instruction 'PVM_SYNTHETIC_CPUID'. This is a necessary step in preparing
    to initialize the PVM guest during booting.
    
    Signed-off-by: Lai Jiangshan <jiangshan.ljs@antgroup.com>
    Signed-off-by: Hou Wenlong <houwenlong.hwl@antgroup.com>

---
+++ arch/x86/include/asm/pvm_para.h
@@ -0,0 +1,66 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _ASM_X86_PVM_PARA_H
+#define _ASM_X86_PVM_PARA_H
+
+#include <linux/init.h>
+#include <uapi/asm/pvm_para.h>
+
+#ifdef CONFIG_PVM_GUEST
+#include <asm/irqflags.h>
+#include <uapi/asm/kvm_para.h>
+
+void __init pvm_early_setup(void);
+
+static inline void pvm_cpuid(unsigned int *eax, unsigned int *ebx,
+			     unsigned int *ecx, unsigned int *edx)
+{
+	asm(__ASM_FORM(.byte PVM_SYNTHETIC_CPUID ;)
+		: "=a" (*eax),
+		  "=b" (*ebx),
+		  "=c" (*ecx),
+		  "=d" (*edx)
+		: "0" (*eax), "2" (*ecx));
+}
+
+/*
+ * pvm_detect() is required to be called before event handling is set up, but
+ * it might also be possibly called under any hypervisor other than PVM.
+ *
+ * Any hypervisor that places the guest kernel in a state where the underlying
+ * interrupt flag is enabled and the underlying CS is 3 should intercept
+ * the CPUID instruction in the PVM_SYNTHETIC_CPUID, which must not trigger
+ * any trap according to the corresponding [paravirtual] architecture.
+ */
+static inline bool pvm_detect(void)
+{
+	unsigned long cs;
+	uint32_t eax, signature[3];
+
+	/* check underlying interrupt flags */
+	if (arch_irqs_disabled_flags(native_save_fl()))
+		return false;
+
+	/* check underlying CS */
+	asm volatile("mov %%cs,%0\n\t" : "=r" (cs) : );
+	if ((cs & 3) != 3)
+		return false;
+
+	/* check the KVM_SIGNATURE leaf */
+	eax = KVM_CPUID_SIGNATURE;
+	pvm_cpuid(&eax, &signature[0], &signature[1], &signature[2]);
+	if (memcmp(KVM_SIGNATURE, signature, 12))
+		return false;
+
+	/*
+	 * PVM is the only KVM variant that places the guest kernel in
+	 * a state where the underlying IF == 1 and underlying CS == 3.
+	 */
+	return true;
+}
+#else
+static inline void pvm_early_setup(void)
+{
+}
+#endif /* CONFIG_PVM_GUEST */
+
+#endif /* _ASM_X86_PVM_PARA_H */
