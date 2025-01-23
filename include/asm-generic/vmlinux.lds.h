d85d1c3b98e689a68440c989ff278ed9f98e5f80
Brian Gerst <brgerst@gmail.com>
2025-01-23 14:07:44 -0500

    percpu: Remove PER_CPU_FIRST_SECTION
    commit 95b0916118106054e1f3d5d7f8628ef3dc0b3c02 upstream.
    
    x86-64 was the last user.
    
    Signed-off-by: Brian Gerst <brgerst@gmail.com>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Reviewed-by: Ard Biesheuvel <ardb@kernel.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Link: https://lore.kernel.org/r/20250123190747.745588-13-brgerst@gmail.com
    Signed-off-by: Hou Wenlong <houwenlong.hwl@antgroup.com>

--- include/asm-generic/vmlinux.lds.h
+++ include/asm-generic/vmlinux.lds.h
@@ -1034,7 +1034,6 @@
  */
 #define PERCPU_INPUT(cacheline)						\
 	__per_cpu_start = .;						\
-	*(.data..percpu..first)						\
 	. = ALIGN(PAGE_SIZE);						\
 	*(.data..percpu..page_aligned)					\
 	. = ALIGN(cacheline);						\
